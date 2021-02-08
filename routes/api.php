b7c0c351859668af9e7a69ed767b004bebcadd73
Marjan Shuplinoski <marjan.shuplinoski@gmail.com>
2021-02-08 11:53:29 +0100

    Api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('editCustomer/{id}', [CustomerController::class, 'editCustomer']);
+Route::post('deposit/{id}/{sum}', [CustomerController::class, 'deposit']);
+Route::get('withdraw/{id}/{sum}', [CustomerController::class, 'withdraw']);
+Route::get('report', [CustomerController::class, 'report']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
