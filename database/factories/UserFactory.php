b42ae062e9802ef265bf506b16e5e53fa180e7bb
Marjan Shuplinoski <marjan.shuplinoski@gmail.com>
2021-02-08 11:54:16 +0100

    Seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,8 +22,19 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $gender = $this->faker->randomElement(['male','female']);
+
+        if ($gender == 'male')
+            $firstname = $this->faker->firstnameMale;
+        else
+            $firstname = $this->faker->firstNameFemale;
+
         return [
-            'name' => $this->faker->name,
+            'first_name' => $firstname,
+            'last_name' => $firstname,
+            'gender' => substr($gender,0,1),
+            'country' => $this->faker->countryCode,
+            'bonus' => rand(5,20),
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
